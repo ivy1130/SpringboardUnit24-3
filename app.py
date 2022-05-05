dcb01863a66c9f3e9ecf254e64d06c927a529438
Ivy <ivy@onprem.com>
2022-05-05 14:39:13 -0600

    Completed Part One: Cupcake Model

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, render_template,  redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+# from forms import 
+
+app = Flask(__name__)
+# app.debug=True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "ughhhh"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake"""
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default='https://tinyurl.com/demo-cupcake')
+    
+    def __repr__(self):
+        c = self
+        return f"<Cupcake id={c.id} Flavor={c.flavor} Size={c.size} Rating={c.rating} Image={c.image}>"
