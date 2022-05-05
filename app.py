41f4bc9470dab10233d34184054ce588b3649b63
Ivy <ivy@onprem.com>
2022-05-05 15:23:43 -0600

    Completed Part Three: Update & Delete Cupcakes

--- app.py
+++ app.py
@@ -39,7 +39,29 @@ def get_cupcake(cupcake_id):
 def create_cupcake():
     """Creates a new cupcake and returns JSON of that created cupcake"""
     new_cupcake = Cupcake(flavor=request.json["flavor"], size=request.json["size"], rating=request.json["rating"], image=request.json["image"] or 'https://tinyurl.com/demo-cupcake')
+    # If there is no image, I'm just using the or statement to set a default. In the models, that link is set as the default image, but when making a post request in which image is "", it saves that empty string to the db instead of the default link. Is there another or better way to do this?
     db.session.add(new_cupcake)
     db.session.commit()
     response_json = jsonify(cupcake=new_cupcake.serialize())
     return (response_json, 201)
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """Updates a particular cupcake and responds w/ JSON of that updated cupcake"""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["DELETE"])
+def delete_cupcate(cupcake_id):
+    """Deletes a particular cupcake"""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message=f"deleted {cupcake}")
